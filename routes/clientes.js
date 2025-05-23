94ea577e068c134a8d5f71a768fdbcacd889db4a
SamuelReyes6671 <samuel.reyes.condori@email.com>
2025-05-23 09:52:37 -0500

    cambio de nombre de usuarios a clientes

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,7 +1,7 @@
 const express= require("express");
 const router = express.Router();
 
-const usersController= require("../controllers/users");
+const usersController= require("../controllers/clientes");
 
 router.get("/", usersController.getAll);
 
