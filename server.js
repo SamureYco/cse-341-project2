62af1a7733ecf2e413598aab164f7d5127876434
SamuelReyes6671 <samuel.reyes.condori@email.com>
2025-05-31 19:03:22 -0500

    Actualizando server

--- server.js
+++ server.js
@@ -1,5 +1,98 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongodb = require("./data/database");
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
+const cors = require("cors");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
+    })
+  )
+  .use(passport.initialize())
+  .use(passport.session())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "POST, GET, PUT, PATCH, OPTIONS, DELETE"
+    );
+    next();
+  })
+  .use(cors({ methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"] }))
+  .use(cors({ origin: "*" }))
+  .use("/", require("./routes/index.js"))
+  .use("/api-docs", require("./routes/swagger"));
+
+// Passport GitHub strategy
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Ruta raíz corregida para enviar mensaje de estado
+app.get("/", (req, res) => {
+  const message =
+    req.session.user !== undefined
+      ? "Logged in as " + req.session.user.displayName
+      : "Logged Out";
+  res.send(message);
+});
+
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-docs",
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/");
+  }
+);
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log("Database is listening and node Running on port " + port);
+    });
+  }
+});
+
+
+/*const express = require("express");
+const bodyParser = require("body-parser");
 const mongodb= require("./data/database")
 const passport= require("passport");
 const session = require ("express-session");
@@ -77,4 +170,4 @@ mongodb.initDb((err) => {
     else{
         app.listen(port,()=>{console.log("Database is listening and node Running on port "+port)})
     }
-})
+})*/
