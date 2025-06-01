8927a3fb4bbc6ff2484727be155ce5faaf919472
SamuelReyes6671 <samuel.reyes.condori@email.com>
2025-06-01 02:01:26 -0500

    test eliminado

--- test-dotenv.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-console.log("GITHUB_CLIENT_ID:", process.env.GITHUB_CLIENT_ID);
-console.log("GITHUB_CLIENT_SECRET:", process.env.GITHUB_CLIENT_SECRET);
-console.log("CALLBACK_URL:", process.env.CALLBACK_URL);
